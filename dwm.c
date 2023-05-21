52fb6f41b15d15b76167705f6e5a9121dd136846
Natasha Kerensikova <natacha@instinctive.eu>
2023-05-21 13:42:55 +0200

    [patch] merged dwm-sendmoncenter-20210805-138b405f.diff

--- dwm.c
+++ dwm.c
@@ -1657,6 +1657,8 @@ sendmon(Client *c, Monitor *m)
 	detachstack(c);
 	c->mon = m;
 	c->tags = m->tagset[m->seltags]; /* assign tags of target monitor */
+	c->x = c->mon->mx + (c->mon->mw - WIDTH(c)) / 2;
+	c->y = c->mon->my + (c->mon->mh - HEIGHT(c)) / 2;
 	attachbottom(c);
 	attachstack(c);
 	focus(NULL);
